4cf09fa9edda329433afda94a5e950fb5405444e
YScott Domes <scott@musefind.com>
2017-09-29 17:00:14 -0700

    Set up first screen

---
+++ App/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    return <Text>Hello, Navigation!</Text>;
+  }
+}
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+});
+
+export default App;
