33915b44570ada8aaadc529152d83a704f12a44c
YScott Domes <scott@musefind.com>
2017-10-23 18:24:03 -0700

    Added new configuration

--- App/config.js
+++ App/config.js
@@ -1,5 +1,6 @@
 const config = {
-  API_URL: 'http://localhost:5000' //'https://frozen-river-79372.herokuapp.com'
+  API_URL: 'http://localhost:5000', //'https://frozen-river-79372.herokuapp.com'
+  SERVER_URL: 'gagasfad'
 }
 
 export default config
